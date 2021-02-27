119d7b6c0d51657657f46e61d12b2c0c1493817b
Enver Çelik <celiqenver@gmail.com>
2021-02-27 20:38:40 +0300

    navigation component

--- app/src/main/java/com/envercelik/todo/ui/MainActivity.kt
+++ app/src/main/java/com/envercelik/todo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.envercelik.todo
+package com.envercelik.todo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.envercelik.todo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/envercelik/todo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,10 @@
+package com.envercelik.todo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.envercelik.todo.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+
+
+
+}
